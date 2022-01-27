2ae8b2654f1790424fd7aecd88059fe8a7070d92
te <shengteli@caih.com>
2022-01-27 18:25:17 +0800

    commit

--- fileService/service/config.ts
+++ fileService/service/config.ts
@@ -1,4 +1,8 @@
 const config = {
+    jwt: {
+        secret: "hahaha",
+        loginPath: "/oauth/login",
+    },
     appRootDirPath: '/Users/shengte/git/tensorflowjs/fileService',
     appreleaseDirPath: '/test/'
 }
